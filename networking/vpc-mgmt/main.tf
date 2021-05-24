3d729aa02ca08a1815b668d913d9f3c4c072d795
thuynh49 <thuynh49@dxc.com>
2021-05-24 10:07:33 +0700

    update tf

--- networking/vpc-mgmt/main.tf
+++ networking/vpc-mgmt/main.tf
@@ -23,14 +23,14 @@ module "vpc" {
   # Make sure to replace <VERSION> in this URL with the latest module-vpc release
   source = "git@github.com:terraform-aws-modules/terraform-aws-vpc.git?ref=v3.0.0"
 
-  // name = var.name
-  // cidr = var.cidr
+  name = var.name
+  cidr = var.cidr
 
-  // azs             = var.azs
-  // private_subnets = var.private_subnets
-  // public_subnets  = var.public_subnets
+  azs             = var.azs
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
 
-  // enable_nat_gateway = var.enable_nat_gateway
+  enable_nat_gateway = var.enable_nat_gateway
   
 
 }
