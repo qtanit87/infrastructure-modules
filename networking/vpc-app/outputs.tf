c3712f089b24d87a55008e2b26f5a982df60cb4b
thuynh49 <thuynh49@dxc.com>
2021-05-24 16:16:38 +0700

    update tf

---
+++ networking/vpc-app/outputs.tf
@@ -0,0 +1,44 @@
+output "vpc_name" {
+  description = "The VPC name"
+  value       = module.vpc.name
+}
+
+output "vpc_id" {
+  description = "The VPC ID"
+  value       = module.vpc.vpc_id
+}
+
+output "vpc_cidr_block" {
+  description = "The VPC CIDR block"
+  value       = module.vpc.vpc_cidr_block
+}
+
+output "public_subnets_cidr_blocks" {
+  description = "The CIDR blocks of the public subnets"
+  value       = module.vpc.public_subnets_cidr_blocks
+}
+
+output "private_subnets_cidr_blocks" {
+  description = "The CIDR blocks of the private app subnets"
+  value       = module.vpc.private_subnets_cidr_blocks
+}
+
+// output "private_persistence_subnet_cidr_blocks" {
+//   description = "The CIDR blocks of the private persistence subnets"
+//   value       = module.vpc.private_persistence_subnet_cidr_blocks
+// }
+
+output "public_subnets" {
+  description = "The IDs of the public subnets"
+  value       = module.vpc.public_subnets
+}
+
+output "private_subnets" {
+  description = "The IDs of the private app subnets"
+  value       = module.vpc.private_subnets
+}
+
+// output "private_persistence_subnet_ids" {
+//   description = "The IDs of the private persistence subnets"
+//   value       = module.vpc.private_persistence_subnet_ids
+// }
