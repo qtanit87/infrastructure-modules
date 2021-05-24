1dfe8e8178de57425fb9953ad92e468fc667c794
thuynh49 <thuynh49@dxc.com>
2021-05-24 09:19:09 +0700

    update tf

--- networking/vpc-mgmt/variables.tf
+++ networking/vpc-mgmt/variables.tf
@@ -1,14 +1,36 @@
-
-
-variable "aws_region" {
-  description = "Name to be used on region"
+variable "name" {
+  description = "Name to be used on all the resources as identifier"
   type        = string
   default     = ""
 }
 
-variable "aws_account_id" {
-  description = "Name to be used on aws_account_id"
+variable "cidr" {
+  description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overridden"
   type        = string
-  default     = ""
+  default     = "0.0.0.0/0"
+}
+
+variable "azs" {
+  description = "A list of availability zones names or ids in the region"
+  type        = list(string)
+  default     = []
+}
+
+variable "public_subnets" {
+  description = "A list of public subnets inside the VPC"
+  type        = list(string)
+  default     = []
+}
+
+variable "private_subnets" {
+  description = "A list of private subnets inside the VPC"
+  type        = list(string)
+  default     = []
+}
+
+variable "enable_nat_gateway" {
+  description = "Should be true if you want to provision NAT Gateways for each of your private networks"
+  type        = bool
+  default     = false
 }
 
