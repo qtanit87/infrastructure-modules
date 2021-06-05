64818cb59c6663af4ef37dab4d4a5892afd23058
thuynh49 <thuynh49@dxc.com>
2021-06-05 21:16:21 +0700

    update tf

--- services/eks-cluster/dependencies.tf
+++ services/eks-cluster/dependencies.tf
@@ -1,9 +1,9 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    region = var.terraform_state_aws_region
-    bucket = var.terraform_state_s3_bucket
-    key    = "${var.terraform_state_aws_region}/${var.terraform_state_environment}/networking/vpc-app/terraform.tfstate"
+    region = ap-southeast-1
+    bucket = terra-staging-state
+    key    = "ap-southeast-1/staging/networking/vpc-app/terraform.tfstate"
   }
-    
+  //key    = "${var.terraform_state_aws_region}/${var.terraform_state_environment}/networking/vpc-app/terraform.tfstate"
 }
