4f71cdfb03cd3d02eb7dc74f2729df2fb938b956
thuynh49 <thuynh49@dxc.com>
2021-06-05 09:07:21 +0700

    update tf

--- services/eks-cluster/main.tf
+++ services/eks-cluster/main.tf
@@ -1,9 +1,9 @@
 data "aws_eks_cluster" "cluster" {
-  name = module.my-cluster.cluster_id
+  name = module.eks.cluster_id
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.my-cluster.cluster_id
+  name = module.eks.cluster_id
 }
 
 provider "kubernetes" {
