3b4bbeefc99f8877dd6fa00bf3aa9c6c23edcdd1
thuynh49 <thuynh49@dxc.com>
2021-06-06 20:10:33 +0700

    update tf

--- services/eks-cluster/main.tf
+++ services/eks-cluster/main.tf
@@ -17,35 +17,59 @@ provider "kubernetes" {
 data "aws_availability_zones" "available" {
 }
 
-resource "aws_security_group" "worker_group_mgmt_one" {
-  name_prefix = "worker_group_mgmt_one"
-  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+// resource "aws_security_group" "worker_group_mgmt_one" {
+//   name_prefix = "worker_group_mgmt_one"
+//   vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
 
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+//   ingress {
+//     from_port = 22
+//     to_port   = 22
+//     protocol  = "tcp"
 
-    cidr_blocks = [
-      "10.0.0.0/8",
-    ]
+//     cidr_blocks = [
+//       "10.0.0.0/8",
+//     ]
+//   }
+// }
+
+// resource "aws_security_group" "all_worker_mgmt" {
+//   name_prefix = "all_worker_management"
+//   vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+
+//   ingress {
+//     from_port = 22
+//     to_port   = 22
+//     protocol  = "tcp"
+
+//     cidr_blocks = [
+//       "10.0.0.0/8",
+//       "172.16.0.0/12",
+//       "192.168.0.0/16",
+//     ]
+//   }
+// }
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 2.47"
+
+  name                 = "test-vpc"
+  cidr                 = "172.16.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
+  public_subnets       = ["172.16.4.0/24", "172.16.5.0/24", "172.16.6.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
   }
-}
 
-resource "aws_security_group" "all_worker_mgmt" {
-  name_prefix = "all_worker_management"
-  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
-
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-
-    cidr_blocks = [
-      "10.0.0.0/8",
-      "172.16.0.0/12",
-      "192.168.0.0/16",
-    ]
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
   }
 }
 
@@ -53,35 +77,55 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = var.cluster_name
   cluster_version = "1.20"
-  subnets         = data.terraform_remote_state.vpc.outputs.public_subnets
+  // subnets         = data.terraform_remote_state.vpc.outputs.public_subnets
+  subnets         = module.vpc.private_subnets
   cluster_endpoint_private_access = true
-  vpc_id          = data.terraform_remote_state.vpc.outputs.vpc_id
+  // vpc_id          = data.terraform_remote_state.vpc.outputs.vpc_id
+  vpc_id = module.vpc.vpc_id
 
-  
+  tags = {
+    Environment = "test"
+    GithubRepo  = "terraform-aws-eks"
+    GithubOrg   = "terraform-aws-modules"
+  }
+
+  node_groups_defaults = {
+    ami_type  = "AL2_x86_64"
+    disk_size = 50
+  }  
 
   node_groups = {
-    public = {
-      subnets          = data.terraform_remote_state.vpc.outputs.public_subnets
-      desired_capacity = var.asg_min_size
-      max_capacity     = var.asg_max_size
-      min_capacity     = var.asg_min_size
-
-      instance_type = var.instance_type
-      k8s_labels = {
-        Environment = "public"
-      }
-    }
-    // private = {
-    //   subnets          = data.terraform_remote_state.vpc.outputs.private_subnets
+    // public = {
+    //   subnets          = data.terraform_remote_state.vpc.outputs.public_subnets
     //   desired_capacity = var.asg_min_size
     //   max_capacity     = var.asg_max_size
     //   min_capacity     = var.asg_min_size
 
     //   instance_type = var.instance_type
     //   k8s_labels = {
-    //     Environment = "private"
+    //     Environment = "public"
     //   }
     // }
+
+    private = {
+      subnets          = data.terraform_remote_state.vpc.outputs.private_subnets
+      desired_capacity = var.asg_min_size
+      max_capacity     = var.asg_max_size
+      min_capacity     = var.asg_min_size
+
+      instance_type = var.instance_type
+      k8s_labels = {
+        Environment = "private"
+      }
+      k8s_labels = {
+        Environment = "test"
+        GithubRepo  = "terraform-aws-eks"
+        GithubOrg   = "terraform-aws-modules"
+      }
+      additional_tags = {
+        ExtraTag = "example"
+      }
+    }
   }
 
   // worker_groups = [
